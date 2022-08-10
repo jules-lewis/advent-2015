2f3900cdfde1e39db2935e21f1485e7045c92316
Jules Lewis <juleslewis2000@gmail.com>
2022-08-11 00:22:54 +0100

    Day 3 complete!

---
+++ day03.py
@@ -0,0 +1,120 @@
+'''
+------------------------------------------------------------------------------
+ADVENT OF CODE 2015 - DAY 3: Perfectly Spherical Houses in a Vacuum
+------------------------------------------------------------------------------
+
+Santa is delivering presents to an infinite two-dimensional grid of houses.
+
+He begins by delivering a present to the house at his starting location, and 
+then an elf at the North Pole calls him via radio and tells him where to move 
+next. Moves are always exactly one house to the north (^), south (v), east (>), 
+or west (<). After each move, he delivers another present to the house at his 
+new location.
+
+However, the elf back at the north pole has had a little too much eggnog, and 
+so his directions are a little off, and Santa ends up visiting some houses 
+more than once. How many houses receive at least one present?
+
+For example:
+
+> delivers presents to 2 houses: one at the starting location, and one to 
+the east.
+
+^>v< delivers presents to 4 houses in a square, including twice to the house 
+at his starting/ending location.
+
+^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 
+2 houses.
+
+
+------------------------------------------------------------------------------
+PART 2
+------------------------------------------------------------------------------
+
+The next year, to speed up the process, Santa creates a robot version of 
+himself, Robo-Santa, to deliver presents with him.
+
+Santa and Robo-Santa start at the same location (delivering two presents 
+to the same starting house), then take turns moving based on instructions 
+from the elf, who is eggnoggedly reading from the same script as the previous 
+year.
+
+This year, how many houses receive at least one present?
+
+For example:
+
+^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa 
+goes south.
+
+^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where 
+they started.
+
+^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and 
+Robo-Santa going the other.
+
+
+
+
+'''
+
+import time
+
+#Timing: Start
+start = time.perf_counter()
+
+#Load the puzzle data
+with open('day03.txt') as f:
+    data = f.read().rstrip()
+
+#I think we're going to use a dictionary, keyed on Santa's X and Y co-ords
+santaX = 0
+santaY = 0
+houses = {}
+
+def incrementHouse(x, y, houses):
+    if (x, y) in houses:
+        houses[(x, y)] += 1
+    else:
+        houses[(x, y)] = 1
+
+def calculateMove(x, y, move):
+    if move == '^':
+        return x, y + 1
+    elif move == 'v':
+        return x, y - 1
+    elif move == '>':
+        return x + 1, y
+    else:
+        return x - 1, y
+
+#Part 1
+incrementHouse(santaX, santaY, houses)
+for char in data:
+    santaX, santaY = calculateMove(santaX, santaY, char)
+    incrementHouse(santaX, santaY, houses)
+print(len(houses))
+
+#Part 2
+santaX = 0
+santaY = 0
+robX = 0
+robY = 0
+houses = {}
+incrementHouse(santaX, santaY, houses)
+incrementHouse(robX, robY, houses)
+moves = 0
+for char in data:
+    moves += 1
+    #Odd moves are Santa, even are Robot
+    if (moves % 2) == 0:
+        robX, robY = calculateMove(robX, robY, char)
+        incrementHouse(robX, robY, houses)
+    else:
+        santaX, santaY = calculateMove(santaX, santaY, char)
+        incrementHouse(santaX, santaY, houses)
+print(len(houses))
+
+
+#Timing: End
+end = time.perf_counter()
+print(f"Time to complete = {str((end-start)*1000)} milliseconds.")
